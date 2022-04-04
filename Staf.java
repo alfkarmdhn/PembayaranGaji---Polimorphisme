86108b4a41e56dc32d1ab1f9e7eb122eb3f8f214
Mohammad Alka. R <99939707+alfkarmdhn@users.noreply.github.com>
2022-04-04 20:21:42 +0700

    Create Staf.java

---
+++ Staf.java
@@ -0,0 +1,16 @@
+
+package polimorphisme_latihan3;
+
+public class Staf extends Pegawai{
+       private static final int gajiStaf=50000;
+    private static final int bonusStaf=10000;
+    
+    @Override
+    public int gaji(){
+        return gajiStaf;
+    }
+    public int Bonus(){
+        return bonusStaf;
+    }
+}
+ 
