5a87d0afd29f2109ce03c1633f3f383e0f964cb9
Mohammad Alka. R <99939707+alfkarmdhn@users.noreply.github.com>
2022-04-04 20:20:16 +0700

    Create Pegawai.java

---
+++ Pegawai.java
@@ -0,0 +1,19 @@
+
+package polimorphisme_latihan3;
+
+public class Pegawai {
+    String nama;
+    int gaji;
+    Pegawai(){
+        
+    }
+    Pegawai (String nm){
+        this.nama=nm;
+        System.out.println("pegawai");
+    }
+    public int gaji(){
+        return 0;
+    }
+}
+
+
